eae925e1c5ae8771502954379752de62aa3934a9
Ramiel <r4miel05@pm.me>
2025-04-09 12:04:04 +1000

    removed meta data and template variables

--- src/lib/variables.ts
+++ src/lib/variables.ts
@@ -1,32 +1,14 @@
 // Set any item to undefined to remove it from the site or to use the default value
-// TODO these should mostly become i18n strings
+// TODO these should mostly become i18n strings.
+// TODO a lot of these are just the template projects values. clean them up.
 export const GLOBAL = {
   // Site metadata
   alias: "Ramiel",
   avatar: "ramiel.webp",
-  rootUrl: "https://foo.todooooo",
-  shortDescription: "Retro-Inspired Theme &<br /> Built for Astro",
-  longDescription: "Zaggonaut is a retro-inspired theme for Astro, built using TypeScript, TailwindCSS, and Astro.",
+  rootUrl: "https://a-new-golden-page.fly.dev/",
+  shortDescription: "Ramiel's Personal Site",
+  longDescription: "A site born of nostalgia for the Small Web. Here you will find Ramiel's thoughts and projects.",
 
   // Other site links
   githubProfile: "https://github.com/ramiel05",
-
-  // Common text names used throughout the site
-  postsName: "fragments",
-  projectsName: "projects",
-  viewAll: "View All",
-
-  // Common descriptions used throughout the site
-  noPosts: "No featured articles yet.",
-  noProjects: "No featured projects yet.",
-
-  // Blog metadata
-  blogTitle: "My Thoughts & Takes",
-  blogShortDescription: "Practical wisdom, unfiltered thoughts, and hot takes.",
-  blogLongDescription: "Web development, tech trends, and the occasional programming mishap.",
-
-  // Project metadata
-  projectTitle: "Projects and Code",
-  projectShortDescription: "A list of my web development projects and developer tools.",
-  projectLongDescription: "All of my projects, including both frontend and full-stack applications.",
 };
